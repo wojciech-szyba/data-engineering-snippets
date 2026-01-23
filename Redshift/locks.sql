0501e81dea1b1ef472332ca4a59be69971fdaafa
Wojciech Szyba <151162903+wojciech-szyba@users.noreply.github.com>
2026-01-23 14:00:07 +0100

    formating

--- Redshift/locks.sql
+++ Redshift/locks.sql
@@ -1,5 +1,5 @@
-select *,datediff(s,txn_start,getdate())/86400||' days '||datediff(s,txn_start,getdate())%86400/3600||' hrs '||datediff(s,txn_start,getdate())%3600/60||' mins '||datediff(s,txn_start,getdate())%60||' secs' as "duration"
-from svv_transactions 
-where lockable_object_type='transactionid' 
-  and pid<>pg_backend_pid() 
-order by 3;
+SELECT *,datediff(s,txn_start,getdate())/86400||' days '||datediff(s,txn_start,getdate())%86400/3600||' hrs '||datediff(s,txn_start,getdate())%3600/60||' mins '||datediff(s,txn_start,getdate())%60||' secs' AS "duration"
+FROM svv_transactions 
+WHERE lockable_object_type='transactionid' 
+  AND pid<>pg_backend_pid() 
+ORDER BY 3;
